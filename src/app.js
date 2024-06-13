78d0beab418da50334fad947b9c85520887958d8
Serhii Illarionov <sergio.illarionov@gmail.com>
2024-06-13 16:12:13 +0300

    add: CI config - add deploy config

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 import {CustomSelect} from './components/custom-select';
 import './styles/main.scss';
 
-const options = Array.from({ length: 100000 }, (_, index) => `Option ${index + 1}`);
+const options = Array.from(
+  {length: 100000},
+  (_, index) => `Option ${index + 1}`,
+);
 
 const App = () => {
-  const handleSelectChange = (selectedOption) => {
-    console.log('Selected option:', selectedOption);
-  };
+  const handleSelectChange = () => {};
 
   return (
     <div className="App">
       <h1>Custom Select Component</h1>
       <div style={{width: '300px'}}>
-        <CustomSelect options={options} onChange={handleSelectChange}/>
+        <CustomSelect options={options} onChange={handleSelectChange} />
       </div>
     </div>
   );
