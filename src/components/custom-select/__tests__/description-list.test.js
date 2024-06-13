8abd0512f0643dd4a1245cd515d4987f7b2b1afa
Serhii Illarionov <sergio.illarionov@gmail.com>
2024-06-13 12:40:51 +0300

    add: tests for CustomSelect component

---
+++ src/components/custom-select/__tests__/description-list.test.js
@@ -0,0 +1,119 @@
+import {render, screen} from '@testing-library/react';
+import {userEvent} from '@testing-library/user-event';
+import {CustomSelect} from '../custom-select';
+
+const SELECT_LABEL = 'Select an option'
+const OPTIONS = [
+  'Option 1',
+  'Option 2',
+  'Option 3',
+  'Option 4',
+  'Option 5',
+]
+
+const getComponent = (props) => (
+  <CustomSelect
+    {...{
+      options: OPTIONS,
+      onChange: jest.fn(),
+      ...props,
+    }}
+  />
+);
+
+describe('`CustomSelect` component', () => {
+  it('should render component with `Select an option` label', () => {
+    render(getComponent());
+
+    expect(screen.getByText(SELECT_LABEL)).toBeInTheDocument();
+  });
+
+  describe('when select was selected with `Tab` key', () => {
+    beforeEach(async () => {
+      render(getComponent());
+      await userEvent.tab();
+    });
+
+
+    it('should change select state to focus', async () => {
+      expect(screen.getByText(SELECT_LABEL)).toHaveFocus();
+    });
+
+    describe('when `ArrowUp` key was clicked', () => {
+      it('should open popup of select with items', async () => {
+        await userEvent.keyboard('{arrowup}');
+
+        expect(screen.getByText(OPTIONS[0])).toBeInTheDocument();
+      });
+    });
+
+    describe('when `ArrowDown` key was clicked', () => {
+      beforeEach(async () => {
+        await userEvent.keyboard('{arrowdown}');
+      });
+
+      it('should open popup of select with items', async () => {
+        expect(screen.getByText(OPTIONS[0])).toBeInTheDocument();
+      });
+
+      describe('when `Option 2` was chosen  with `ArrowDown` key', () => {
+        beforeEach(async () => {
+          await userEvent.keyboard('{arrowdown}');
+        });
+
+        describe('when `Enter` key was clicked', () => {
+          beforeEach(async () => {
+            await userEvent.keyboard('{enter}');
+          });
+
+          it('should close popup of select with items', () => {
+            expect(screen.queryByText(OPTIONS[0])).not.toBeInTheDocument();
+          });
+
+          it('should render `Option 2` as label of select', () => {
+            expect(screen.getByText(OPTIONS[1])).toBeInTheDocument();
+          });
+        });
+      });
+    });
+  });
+
+  describe('when select was clicked', () => {
+    beforeEach(async () => {
+      render(getComponent());
+      await userEvent.click(screen.getByText(SELECT_LABEL));
+    });
+
+
+    it('should open popup of select with items', async () => {
+      expect(screen.getByText(OPTIONS[0])).toBeInTheDocument();
+    });
+
+    describe('when option `Option 4` was clicked', () => {
+      beforeEach(async () => {
+        await userEvent.hover(screen.getByText(OPTIONS[3]));
+        await userEvent.keyboard('{enter}');
+      });
+
+      it('should close popup of select with items', async () => {
+        expect(screen.queryByText(OPTIONS[0])).not.toBeInTheDocument();
+      });
+
+      it('should render chosen label as label of select', () => {
+        expect(screen.getByText(OPTIONS[3])).toBeInTheDocument();
+      });
+    });
+
+    describe('when `Esc` key was clicked', () => {
+      it('should close popup of select with items', async () => {
+        await userEvent.keyboard('{Escape}');
+
+        expect(screen.queryByText(OPTIONS[0])).not.toBeInTheDocument();
+      });
+
+      it('should change select state to focus', async () => {
+        expect(screen.getByText(SELECT_LABEL)).toHaveFocus();
+      });
+    });
+  });
+});
